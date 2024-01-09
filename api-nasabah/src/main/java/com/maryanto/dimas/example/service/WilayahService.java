3a91582d68312141136d99dcea8aa2f0736d4655
dimasm93 <software.dimas_m@icloud.com>
2024-01-09 11:39:56 +0700

    update configuration

--- api-nasabah/src/main/java/com/maryanto/dimas/example/service/WilayahService.java
+++ api-nasabah/src/main/java/com/maryanto/dimas/example/service/WilayahService.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "wilayah-api")
+@FeignClient(name = "api-wilayah")
 public interface WilayahService {
 
     @GetMapping("/api/kota/findById/{kotaId}")
